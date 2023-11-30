d7fddc4354f0bd7b4707fdc0af8f6c2c6c092627
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 14:46:50 +0530

    Update customer_age_segmentation.sql

--- models/analysis/customer_age_segmentation.sql
+++ models/analysis/customer_age_segmentation.sql
@@ -1,9 +1,13 @@
 {{ config(materialized='view') }}
 
 select 
-	age, 
-	count(*) 
+  case
+    when age between 18 and 30 then 'Young Adults'
+    when age between 31 and 60 then 'Adults'
+    when age between 61 and 75 then 'Senior Citizens'
+  end as age_bracket,
+  count(*) as total_customers
 from 
-	icebase.retail.customer_360_final 
-group by 1 
-order by 1
+  icebase.retail.customer_360_final
+group by 1
+order by 2 desc
