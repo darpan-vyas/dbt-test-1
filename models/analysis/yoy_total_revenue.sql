8832f5e46ce752b6ad664484de8aeaa2f4e578af
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 14:17:33 +0530

    Create yoy_total_revenue.sql

---
+++ models/analysis/yoy_total_revenue.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view') }}
+
+select 
+	date_format(order_date, '%Y'), 
+  count(distinct customer_id) as total_customers,
+  count(distinct order_id) as total_orders,
+	sum(order_amount) as total_revenue 
+from 
+	icebase.retail.orders_enriched 
+group by 1 
+order by 1
