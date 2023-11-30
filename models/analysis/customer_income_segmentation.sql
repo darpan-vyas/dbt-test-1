2315f1941abdbce2250c98c390b967b30917e6e7
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 13:58:46 +0530

    Update customer_income_segmentation.sql

--- models/analysis/customer_income_segmentation.sql
+++ models/analysis/customer_income_segmentation.sql
@@ -1,3 +1,3 @@
 {{ config(materialized='view') }}
 
-select annual_income, count(*) as total_customers from icebase.pyflaresdk.sdk_sample_dataset group by 1 order by 1 
+select annual_income, count(*) from icebase.retail.customer group by 1 order by 1 desc
