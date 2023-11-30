d3773993297d51b200e9bd96e3a49a22b05039e8
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 13:59:24 +0530

    Update customer_income_segmentation.sql

--- models/analysis/customer_income_segmentation.sql
+++ models/analysis/customer_income_segmentation.sql
@@ -1,3 +1,3 @@
 {{ config(materialized='view') }}
 
-select annual_income, count(*) from icebase.retail.customer group by 1 order by 1 desc
+select annual_income, count(*) from icebase.retail.customer_360_final group by 1 order by 1 desc
