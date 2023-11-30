4b50b04de3d3724dbffc819a022c8e3746530bbd
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 14:15:25 +0530

    Create total_revenue_by_state.sql

---
+++ models/analysis/total_revenue_by_state.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='view') }}
+
+select 
+  state, 
+  count(distinct customer_id) as total_customers,
+  count(distinct order_id) as total_orders, 
+  sum(order_amount) as total_revenue
+from 
+  icebase.retail.orders_enriched 
+where 
+  brand_name is not null 
+group by 1 
+order by 4 desc
