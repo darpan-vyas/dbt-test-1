f99c98d40068e127fbd1db2ddb38db3e6920fe84
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 14:10:17 +0530

    Update brand_sale_analysis.sql

--- models/analysis/brand_sale_analysis.sql
+++ models/analysis/brand_sale_analysis.sql
@@ -1,3 +1,13 @@
 {{ config(materialized='view') }}
 
-select brand_name, count(distinct order_id) as total_orders, sum(order_amount) as total_revenue from icebase.retail.orders_enriched where brand_name is not null group by 1 order by 3 desc
+select 
+    brand_name,
+    count(distinct customer_id) as total_customers,
+    count(distinct order_id) as total_orders, 
+    sum(order_amount) as total_revenue 
+from 
+    icebase.retail.orders_enriched 
+where 
+    brand_name is not null 
+group by 1 
+order by 3 desc
