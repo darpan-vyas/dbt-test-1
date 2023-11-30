cd2647e49c61f5ad3657249b45ffd9c756c92900
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 14:49:37 +0530

    Update customer_age_segmentation_sanity_check.sql

--- tests/customer_age_segmentation_sanity_check.sql
+++ tests/customer_age_segmentation_sanity_check.sql
@@ -4,7 +4,7 @@ from
     {{ ref('customer_age_segmentation')}}
 where
     age_bracket is null or
-    age_bracket not in ('Adults','Children','Senior Citizens','Super Senior Citizens','Young Adults') or
+    age_bracket not in ('Young Adults','Adults','Senior Citizens') or
     total_customers < 0 or
     total_orders < 0 or
     total_revenue < 0
