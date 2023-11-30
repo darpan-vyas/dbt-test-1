a933f0932324d97a09d1ffbb73db23480fb0d378
darpan-vyas <36385113+darpan-vyas@users.noreply.github.com>
2023-11-30 15:56:56 +0530

    Update yoy_total_revenue.sql

--- models/analysis/yoy_total_revenue.sql
+++ models/analysis/yoy_total_revenue.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 
 select 
-	date_format(order_date, '%Y'), 
+	date_format(order_date, '%Y') as year, 
   count(distinct customer_id) as total_customers,
   count(distinct order_id) as total_orders,
 	sum(order_amount) as total_revenue 
